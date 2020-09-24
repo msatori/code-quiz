bc308b8f8ac204b4ad24060fe1da88c282de30f9
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-09-24 13:48:05 -0500

    created array for questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,65 @@
 //Variables for Quiz
-var questions
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById("score");
+var submitButton = document.getElementById("submit");
+//Create objects within an array for multiple choice questions
+var codeQuestions = [
+    {
+        question: "Commonly used data types do NOT inclue:",
+        answers: {
+            1: "strings",
+            2: "booleans",
+            3: "alerts",
+            4: "numbers",
+     },
+         rightAnswer: "3"
+    
+    },
+    {
+        question: "Arrays in JavScript can be used to store _________.",
+        answers:{
+            1: "numbers and strings",
+            2: "other arrays",
+            3: "booleans",
+            4: "all of the above",
+        },
+        rightAnswer: ""
+    },
+    { 
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        answers: { 
+            1: "commas",
+            2: "curly brackets",
+            3: "quotes",
+            4: "parenthesis",
+    },
 
+    rightAnswer: ""
+    },
+{
+        question: "A very useful school use during the development and debugging for printing content to the debugger is:",
+        answers: { 
+        1: "JavaScript",
+        2: "terminal/bash",
+        3: "for loops",
+        4: "console.log",
+},
+    rightAnswer: "",
+},
+
+];
+
+function buildQuiz() {
+
+};
+
+function showScore () {
+;
+}
+
+
+//displayquiz
+buildQuiz();
 //GIVEN I am taking a code quiz
 //WHEN I click the start button
 //THEN a timer starts and I am presented with a question
