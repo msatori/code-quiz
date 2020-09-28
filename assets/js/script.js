fb7e0b9b57dc9d401ca2e7b953a763dbad33b81b
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-09-27 21:01:17 -0500

    added seperate JS for questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,143 +12,101 @@ var i = 0;
 var timeLeft = 75;
 var score = 0;
 
+
 //create function to start quiz    
-var startQuiz = function(){
-//hide questions for front page w/submit button
-function setTimer() {
-    var timerInt = setInterval(function(){
-        timeLeft--;
-        timer.textContent = "Timer" + timeLeft;
+var startQuiz = function () {
+    //hide questions for front page w/submit button
+    function setTimer() {
+        var timerInt = setInterval(function () {
+            timeLeft--;
+            timer.textContent = "Timer" + timeLeft;
 
-        if (timeLeft === 0) {
-        endQuiz();
-        }
-        else if (i === codeQuestions.length) {
-            clearInterval(timerInt);
-        }
-    }, 1000)
-    return (score)
+            if (timeLeft === 0) {
+                endQuiz();
+                alert("You're Outta Here")
+            }
+            else if (i === codeQuestions.length) {
+                clearInterval(timerInt);
+            }
+        }, 1000)
+        return (score)
     }
-//Create objects within an array for multiple choice questions
-    var codeQuestions = [
-        {
-            question: "Commonly used data types do NOT inclue:",
-            answers: [
-                "strings",
-                "booleans",
-                "alerts",
-                "numbers",
-            ],
-             rightAnswer: "c"
-        
-        },
-        {
-            question: "Arrays in JavScript can be used to store _________.",
-            answers:[
-                "numbers and strings",
-                "other arrays",
-                "booleans",
-                "all of the above",
-                ],
-            rightAnswer: "d"
-        },
-        { 
-            question: "String values must be enclosed within ______ when being assigned to variables.",
-            answers:[ 
-                "commas",
-                "curly brackets",
-                 "quotes",
-                 "parenthesis",
-             ],
-        rightAnswer: "d"
-        },
-        {
-            question: "A very useful school use during the development and debugging for printing content to the debugger is:",
-            answers: [
-                 "JavaScript",
-                 "terminal/bash",
-                 "for loops",
-                 "console.log",
-            ],
-        rightAnswer: "d",
-        }
-      
-    ];
-    console.log(codeQuestions);
-   
-//create a function to generate one question at a time
-function showQuestions() {
-    answerA.hidden = false;
-    answerB.hidden = false;
-    answerC.hidden = false;
-    answerD.hidden = false;
-//create element to display the question and the answers
-if (i === codeQuestions.length) {
-    //restart quiz
-    quizReset();
-}
-else {
-//assign text content to the questions and answers
-document.getElementById("questions").textContent = codeQuestions[i]["question"];
-document.getElementById("answerA").textContent = codeQuestions[i]["answers"][0];
-document.getElementById("answerB").textContent = codeQuestions[i]["answers"][1];
-document.getElementById("answerC").textContent = codeQuestions[i]["answers"][2];
-document.getElementById("answerD").textContent = codeQuestions[i]["answers"][3];
-}
-//check to see if question was correct
-document.getElementById("answerA").addEventListener("click", function () {
-    if (codeQuestions[i]["question"][0]=== codeQuestions[i]["rightAnswer"]) {
-        feedback.textContent = "Correct!";
-        score ++;
-    }
-    else {
-        feedback.textContent = "Incorrect!";
-    }
-    i++;
-    showQuestions();
-})
 
+
+    //create a function to generate one question at a time
+    function showQuestions() {
+       
+        //create element to display the question and the answers
+        if (i === codeQuestions.length) {
+            //restart quiz
+            quizReset();
+        }
+        else {
+            //assign text content to the questions and answers
+            document.getElementById("questions").textContent = codeQuestions[i]["question"];
+            document.getElementById("answerA").textContent = codeQuestions[i]["answers"][0];
+            document.getElementById("answerB").textContent = codeQuestions[i]["answers"][1];
+            document.getElementById("answerC").textContent = codeQuestions[i]["answers"][2];
+            document.getElementById("answerD").textContent = codeQuestions[i]["answers"][3];
+        }
+        //check to see if question was correct
+        document.getElementById("answerA").addEventListener("click", function () {
+            if (codeQuestions[0].question , codeQuestions.answers[0] === codeQuestions[0].rightAnswer) {
+                feedback.textContent = "Correct!";
+                score++;
+            }
+            else {
+                feedback.textContent = "Incorrect!";
+                timeLeft -= 10;
+            }
+            i++;
+            showQuestions();
+        })
+
+    };
+
+    document.getElementById("answerA").addEventListener("click", function () {
+        if (codeQuestions[1].question , codeQuestions.answers[1] === codeQuestions[1].rightAnswer) {
+            feedback.textContent = "Correct!";
+            score++;
+        }
+        else {
+            feedback.textContent = "Incorrect!";
+            timeLeft -= 10;
+        }
+        i++;
+        showQuestions();
+    })
+    document.getElementById("answerA").addEventListener("click", function () {
+        if (codeQuestions[2].question, codeQuestions.answers[2] === codeQuestions[2].rightAnswer) {
+            feedback.textContent = "Correct!";
+            score++;
+        }
+        else {
+            feedback.textContent = "Incorrect!";
+            timeLeft -= 10;
+        }
+        i++;
+        showQuestions();
+    })
+
+    document.getElementById("answerA").addEventListener("click", function () {
+        if (codeQuestions[3].question , codeQuestions.answers[3] === codeQuestions[3].rightAnswer) {
+            feedback.textContent = "Correct!";
+            score++;
+        }
+        else {
+            feedback.textContent = "Incorrect!";
+            timeLeft -= 10;
+        }
+        i++;
+        showQuestions();
+    })
+    document.getElementById("startButton").addEventListener("click", setTimer);
+    document.getElementById("startButton").addEventListener("click", showQuestions);
 };
 
-document.getElementById("answerA").addEventListener("click", function () {
-    if (codeQuestions[i]["question"][1]=== codeQuestions[i]["rightAnswer"]) {
-        feedback.textContent = "Correct!";
-        score ++;
-    }
-    else {
-        feedback.textContent = "Incorrect!";
-    }
-    i++;
-    showQuestions();
-})
-document.getElementById("answerA").addEventListener("click", function () {
-    if (codeQuestions[i]["question"][2]=== codeQuestions[i]["rightAnswer"]) {
-        feedback.textContent = "Correct!";
-        score ++;
-    }
-    else {
-        feedback.textContent = "Incorrect!";
-    }
-    i++;
-    showQuestions();
-})
-
-document.getElementById("answerA").addEventListener("click", function () {
-    if (codeQuestions[i]["question"][3]=== codeQuestions[i]["rightAnswer"]) {
-        feedback.textContent = "Correct!";
-        score ++;
-    }
-    else {
-        feedback.textContent = "Incorrect!";
-    }
-    i++;
-    showQuestions();
-})
-document.getElementById("startButton").addEventListener("click", showQuestions);
-document.getElementById("startButton").addEventListener("click", setTimer);
-};
-
-function endQuiz(){
+function endQuiz() {
     var initialInput = document.createElement("input");
     initialInput = ("type", "required");
 
@@ -173,7 +131,7 @@ startQuiz();
     //3. decremenet goes down when question is answered incorrectly
 
 
-    
+
 
 
 
